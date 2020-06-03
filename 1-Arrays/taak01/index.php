9d2420b36b495b88d7c227456e9e6b56aebd9d58
Randy Ofosu <randy_ofosu@hotmail.com>
2020-06-03 02:19:45 +0200

    Taak 1 PHP advanced

---
+++ 1-Arrays/taak01/index.php
@@ -0,0 +1,34 @@
+<?php
+
+$Piet = array(
+    "voornaam" => "Piet",
+    "achternaam" => "Kralenbeek",
+    "leeftijd" => 24,
+    "klas" => "4a"
+);
+
+$Mohammed = array(
+    "voornaam" => "Mohammed",
+    "achternaam" => "Somali",
+    "leeftijd" => 15,
+    "klas" => "1b"
+);
+
+$Els = array(
+    "voornaam" => "Els",
+    "achternaam" => "Bond",
+    "leeftijd" => 17,
+    "klas" => "3c"
+);
+
+echo "Gegevens Piet: "."Voornaam: ".$Piet["voornaam"]." Achternaam: ".$Piet["achternaam"]." Leeftijd: ".$Piet["leeftijd"]." Klas: ".$Piet["klas"];
+
+Echo "<br>";
+
+echo "Gegevens Mohammed: "."Voornaam: ".$Mohammed["voornaam"]." Achternaam: ".$Mohammed["achternaam"]." Leeftijd: ".$Mohammed["leeftijd"]." Klas: ".$Mohammed["klas"];
+
+Echo "<br>";
+
+echo "Gegevens Els: "."Voornaam: ".$Els["voornaam"]." Achternaam: ".$Els["achternaam"]." Leeftijd: ".$Els["leeftijd"]." Klas: ".$Els["klas"];
+
+?>
