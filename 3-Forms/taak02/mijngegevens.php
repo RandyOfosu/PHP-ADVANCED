b1eb3124667cc08e8fc235cabdd6e4a8d96d6680
Randy Ofosu <randy_ofosu@hotmail.com>
2020-06-04 19:12:38 +0200

    forms taak 2

---
+++ 3-Forms/taak02/mijngegevens.php
@@ -0,0 +1,11 @@
+<?php
+
+echo $_POST["voornaam"]."<br>";
+echo $_POST["achternaam"]."<br>";
+echo $_POST["klas"]."<br>";
+echo $_POST["leeftijd"]."<br>";
+echo $_POST["adres"]."<br>";
+echo $_POST["plaatsnaam"]."<br>";
+echo $_POST["favorieteMuziekband"]."<br>";
+
+?>
