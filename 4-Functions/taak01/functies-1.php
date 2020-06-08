051f209ae68113b3a34fcae58bc1793a84c7f774
Randy Ofosu <randy_ofosu@hotmail.com>
2020-06-08 16:49:45 +0200

    functions taak 1

---
+++ 4-Functions/taak01/functies-1.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Functions PHP</title>
+</head>
+<body>
+<?php
+
+
+function schrijfKleur($color){
+    echo "<p style='color: $color';>Deze tekst is nu een andere kleur</p>";
+}
+    schrijfKleur("red");
+    schrijfKleur("blue");
+    schrijfKleur("green");
+    schrijfKleur("#FF30109");
+?>   
+
+<p >
+</body>
+</html>
